6391c2cf41cd680aef560806415e4b3dc3225d76
Irene Okafor <okaforirenen@gmail.com>
2026-02-21 11:59:03 +0100

    updates

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-const express = require('express')
-const cors = require('cors')
-const router = require('./routes')
+const express = require("express");
+const cors = require("cors");
+const router = require("./routes");
 
-const app = express()
-app.use(express.json())
-app.use(cors())
-app.use(router)
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(router);
 
-const PORT = process.env.PORT || 3000
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+const PORT = process.env.PORT || 5002;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-module.exports = app
+module.exports = app;
